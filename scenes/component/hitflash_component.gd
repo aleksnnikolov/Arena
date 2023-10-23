773b13da3dff81e7d0f26d4b819bcf5a14f795d7
aleksnnikolov <aleniknikolov@gmail.com>
2023-10-23 21:55:40 +0200

    added a custom font, floating text, hitflash on enemies

---
+++ scenes/component/hitflash_component.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@export var health_component: Node
+@export var sprite: Sprite2D
+@export var hitflash_material: ShaderMaterial
+
+var hitflash_tween: Tween
+
+func _ready():
+	health_component.health_changed.connect(on_health_changed)
+	sprite.material = hitflash_material
+
+
+func on_health_changed():
+	if hitflash_tween != null && hitflash_tween.is_valid():
+		hitflash_tween.kill()
+	
+	(sprite.material as ShaderMaterial).set_shader_parameter("lerp_percent", 1.0)
+	hitflash_tween = create_tween()
+	hitflash_tween.tween_property(sprite.material, "shader_parameter/lerp_percent", 0.0, 0.2)\
+				  .set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	
