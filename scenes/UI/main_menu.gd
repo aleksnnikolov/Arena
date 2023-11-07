e6c121a76a06e3e7b57ba4dfc2b7713eb294d3cb
aleksnnikolov <aleniknikolov@gmail.com>
2023-11-07 12:23:16 +0100

    added main menu, options menu and pause menu

---
+++ scenes/UI/main_menu.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+var options_scene = preload("res://scenes/UI/options_menu.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	%PlayButton.pressed.connect(on_press_play)
+	%OptionsButton.pressed.connect(on_press_options)
+	%QuitButton.pressed.connect(on_press_quit)
+
+
+func on_press_play():
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func on_press_options():
+	var options_instance = options_scene.instantiate()
+	add_child(options_instance)
+	options_instance.back_pressed.connect(on_options_closed.bind(options_instance))
+
+
+func on_options_closed(options_instance: Node):
+	options_instance.queue_free()
+
+
+func on_press_quit():
+	get_tree().quit()
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,9 +3,17 @@ extends Node
 @export var end_screen_scene: PackedScene
 @onready var arena_time_manager: Node = $ArenaTimeManager
 
+var pause_menu_scene = preload("res://scenes/UI/pause_menu.tscn")
+
 func _ready():
 	%Player.health_component.died.connect(on_player_died)
 	arena_time_manager.time_elapsed.connect(on_game_won)
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		get_tree().root.set_input_as_handled()
 
 
 func on_game_won():
