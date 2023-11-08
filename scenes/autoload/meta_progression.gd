837b1d21eab38184e9b96e74b30bb16a19fa749a
aleksnnikolov <aleniknikolov@gmail.com>
2023-11-08 17:09:19 +0100

    implemented base of meta progression + save/load file capability

---
+++ scenes/autoload/meta_progression.gd
@@ -0,0 +1,40 @@
+extends Node
+
+const SAVE_FILE_PATH = "user://game.save"
+
+var save_data: Dictionary = {
+	"meta_upgrades_currency": 0,
+	"meta_upgrades": {}
+}
+
+
+func _ready():
+	GameEvents.experience_drop_collected.connect(on_experience_collected)
+	load_save_file()
+
+
+func load_save_file():
+	if !FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+	
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+
+
+func save():
+	var save_file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	save_file.store_var(save_data)
+
+
+func add_meta_upgrade(upgrade: MetaUpgrade):
+	if !save_data["meta_upgrades"].has(upgrade.id):
+		save_data["meta_upgrades"][upgrade.id] = {
+			"quantity": 0
+		}
+	
+	save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
+
+
+func on_experience_collected(number: float):
+	save_data["meta_upgrades_currency"] += number
+	save()
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -20,9 +20,11 @@ func on_game_won():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
 	end_screen_instance.set_victory()
+	MetaProgression.save()
 
 
 func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
 	end_screen_instance.set_defeat()
+	MetaProgression.save()
